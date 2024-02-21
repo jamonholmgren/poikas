2d3b64de597d82b6805cb6849b5961eea6114876
Jamon Holmgren <code@jamon.dev>
2024-02-20 16:49:47 -0800

    Delete static and add redirect

---
+++ functions/player/[[params]].ts
@@ -0,0 +1,25 @@
+import { getData } from "../../src/data/poikas"
+import { PlayerPage } from "../../src/pages/PlayerPage"
+
+export async function onRequest(context) {
+  const data = getData()
+
+  // grab the slugified name from the ?player= query parameter
+  const fullName = context.params.player
+
+  // Find the player by name
+  const player = data.players.find((p) => p.name === fullName)
+
+  // If we found the player, 301 redirect to /players/:slug
+  if (player) {
+    return new Response(`Redirecting to /players/${player.slug}`, {
+      status: 302,
+      headers: {
+        location: `/players/${player.slug}`,
+      },
+    })
+  } else {
+    // If the player doesn't exist, return a 404
+    return new Response(`Player ${fullName} not found`, { status: 404 })
+  }
+}
